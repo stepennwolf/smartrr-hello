79ccc0a050ba57c94aabf60e42243a05de791fd1
igor.tomov <stepennwolf@gmail.com>
2023-02-08 09:58:17 +0200

    Remove API key.

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const appConfig = {
   currencyApi: {
     endpoint: 'https://currency-converter5.p.rapidapi.com/currency/convert',
-    apiKey: 'a59f55f96emsh840f8e1c197eb20p17c4c5jsn6769391e20cd',
+    apiKey: '<ADD YOUR KEY HERE>',
     currencyFrom: 'USD',
     currencyTo: 'BRL',
   }
